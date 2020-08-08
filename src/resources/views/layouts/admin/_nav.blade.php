89458fe8537901c78618863fa1449fa4365743bc
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-08 12:18:51 -0600

    agregando archivos de frontend

---
+++ src/resources/views/layouts/admin/_nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-md navbar-ligth">
+    <button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="myNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 sidebar"></div>
+                <div class="col-lg-9">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
