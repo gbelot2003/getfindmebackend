3d4eba1361999ef6e0797ef7086a2db6da09ce9e
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-08 14:25:28 -0600

    Agregando menus nav y estilos

--- src/resources/views/layouts/admin/_nav.blade.php
+++ src/resources/views/layouts/admin/_nav.blade.php
@@ -7,18 +7,11 @@
             <div class="row">
                 <div class="col-lg-3 sidebar fixed-top">
                     <a href="/" class="text-white d-block navbar-brand mx-auto text-center mb-4 bottom-border py-3">Tester Application</a>
-                    <div class="bottom-border pb-3">
+                    <div class="bottom-border pb-3 mb-3">
                         <img width="50" class="rounded-circle mr-3" src="https://lh3.googleusercontent.com/proxy/ml7T7XRLo_y2Oh1We7dGPDeyczBxUBXtvfs-7b5A57UrZ3sYvSFgD7LR8AWwAeVBrSNXtNosl24" alt="adminmin">
                         <a href="#/" class="text-white">Gerardo A. Belot</a>
                     </div>
-                    <ul class="navbar-nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <i class="fas fas-home"></i>
-                                Inicio
-                            </a>
-                        </li>
-                    </ul>
+                    @include('layouts.admin._navMenu')
                 </div>
                 <div class="col-lg-9">
                 </div>
