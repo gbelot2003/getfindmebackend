5d50a5b6efc3a30c2fc5f70e626428b358944717
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-08 13:31:04 -0600

    Agregando font-wosome

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -9,17 +9,18 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Monserrat" rel="stylesheet">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     @include('layouts.admin._nav')
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
--- src/resources/views/layouts/admin/_nav.blade.php
+++ src/resources/views/layouts/admin/_nav.blade.php
@@ -5,9 +5,22 @@
     <div class="collapse navbar-collapse" id="myNavbar">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 sidebar"></div>
+                <div class="col-lg-3 sidebar fixed-top">
+                    <a href="/" class="text-white d-block navbar-brand mx-auto text-center mb-4 bottom-border py-3">Tester Application</a>
+                    <div class="bottom-border pb-3">
+                        <img width="50" class="rounded-circle mr-3" src="https://lh3.googleusercontent.com/proxy/ml7T7XRLo_y2Oh1We7dGPDeyczBxUBXtvfs-7b5A57UrZ3sYvSFgD7LR8AWwAeVBrSNXtNosl24" alt="adminmin">
+                        <a href="#/" class="text-white">Gerardo A. Belot</a>
+                    </div>
+                    <ul class="navbar-nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="fas fas-home"></i>
+                                Inicio
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <div class="col-lg-9">
-                
                 </div>
             </div>
         </div>
