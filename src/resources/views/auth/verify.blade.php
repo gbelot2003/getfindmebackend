903f43567a82573784804e23e592dcc4f08bec0e
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-09 21:15:50 -0600

    Imagenes en verify-email

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-body p-0">
 
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                        <div class="col-lg-6 d-none d-lg-block bg-verify-image-2"></div>
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
