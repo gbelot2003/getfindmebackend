fabd47d9b39675327f10915a7858f76d3739eb6c
Gerardo Belot <gbelot2003@hotmail.com>
2020-08-11 10:22:03 -0600

    Agregando estructura de pagina de auditorias

---
+++ src/app/Http/Controllers/Audit/AuditController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Audit;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuditController extends Controller
+{
+    function __construct()
+    {
+        //TODO: Gestionar permisos
+    }
+
+    public function index()
+    {
+
+    }
+
+
+}
